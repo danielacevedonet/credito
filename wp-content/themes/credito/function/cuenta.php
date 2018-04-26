e0e85cc83b10eae8c1f289e8f4c397ea4754b71c
Daniel Acevedo <daniel@aquivamos.org>
2018-04-26 16:12:58 -0500

    Terminamos la funcion de sumar con javascript (Version Estable)

--- wp-content/themes/credito/function/cuenta.php
+++ wp-content/themes/credito/function/cuenta.php
@@ -1,6 +1,12 @@
 <?php
 
 
+//Esta funcion nos crea un Random
+function creditoRan($length = 5)
+{
+    return substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+}
+
 // apartir de aqui el codigo sirve para añadir la venta como un post
 if(isset($_POST['submitted']) && isset($_POST['post_nonce_field']) && wp_verify_nonce($_POST['post_nonce_field'], 'post_nonce')) {
 
@@ -9,6 +15,7 @@ if(isset($_POST['submitted']) && isset($_POST['post_nonce_field']) && wp_verify_
     $fecha_page = trim($_POST['fecha_page']);
     $id_page    = trim($_POST['id_page']);
     $sumar_page = trim($_POST['sumar_page']);
+    $ram_page   = trim($_POST['ram_page']);
 
     $new_post = array(
     'post_title' => wp_strip_all_tags( $_POST['postTitle'] ),
@@ -27,6 +34,7 @@ if(isset($_POST['submitted']) && isset($_POST['post_nonce_field']) && wp_verify_
         update_post_meta($post_id, 'fecha_page', $fecha_page );
         update_post_meta($post_id, 'id_page', $id_page );
         update_post_meta($post_id, 'sumar_page', $sumar_page);
+        update_post_meta($post_id, 'ram_page', $ram_page);
 
         // Redirect
         wp_redirect( get_permalink( $post->post_parent ) ); exit;
